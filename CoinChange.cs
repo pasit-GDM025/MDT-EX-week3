2308136e82d4bc6d56054402dbe3d30d015ec7e8
pasit-GDM025 <110731006+pasit-GDM025@users.noreply.github.com>
2022-09-07 19:38:53 +0700

    Update CoinChange.cs

--- CoinChange.cs
+++ CoinChange.cs
@@ -4,9 +4,7 @@ public class CoinChange
     double[]Coin = {1000,500,100,50,20,10,5,2,1,0.5,0.25}; 
     double count;
     int i;
-    Console.WriteLine("Please input money");
-    money = double.Parse(Console.ReadLine());
-    Console.WriteLine("------------------");
+   
     for(i=0;i<Coin.Length;i++)
     {
         count = money / Coin[i];
