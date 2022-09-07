8fcd4a92123c84017292380559e663f2972b421d
pasit-GDM025 <110731006+pasit-GDM025@users.noreply.github.com>
2022-09-07 21:34:28 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 public class Program
 {
     public static void Main(string[]args)
@@ -13,14 +13,32 @@ public class Program
    string RegistedValue = Console.ReadLine();
    InformationInput myInformation = new InformationInput(ShopName, OwnerNumber, OwnerName, RegistedValue);
    PrintInformationInput (myInformation);
-   Console.WriteLine("Please input money");
-   double money = double.Parse(Console.ReadLine());
+   
+   string coin = InputMoney();
+   double money = ConvertStringToDouble(coin);
    Console.WriteLine("------------------");
    CoinChange bahtChange =  new CoinChange();
    bahtChange.MoneyChange(money);
 }
+public static string InputMoney()
+    {
+        Console.Write("Please Input Money :");
 
- static void PrintInformationInput(InformationInput Information) {
+        return Console.ReadLine();
+    }
+
+    public static double ConvertStringToDouble(string value)
+    {
+        if (double.TryParse(value, out double number))
+        {
+            return number;
+        }
+
+        throw new Exception("Pra trai pi dok yad tood!");
+    }
+
+ static void PrintInformationInput(InformationInput Information) 
+ {
     Console.WriteLine("----------Shop Information----------");
     Console.WriteLine("Shop Name: {0}", Information.ShopName);
     Console.WriteLine("Owner Number: {0}", Information.OwnerNumber);
@@ -28,6 +46,7 @@ public class Program
     Console.WriteLine("Registed value: {0}", Information.RegistedValue);
     Console.WriteLine("-----------------------------------");
 }
-}
 
+
+}
 
