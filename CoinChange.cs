45e4ff17733bd839a12a18667f1205be9e46d7c8
pasit-GDM025 <110731006+pasit-GDM025@users.noreply.github.com>
2022-09-07 21:15:25 +0700

    Update CoinChange.cs

--- CoinChange.cs
+++ CoinChange.cs
@@ -2,14 +2,14 @@ public class CoinChange
 {
     public void MoneyChange(double money){
     double[]Coin = {1000,500,100,50,20,10,5,2,1,0.5,0.25}; 
-    double count;
+    int count;
     int i;
-   
+
     for(i=0;i<Coin.Length;i++)
     {
-        count = money / Coin[i];
+        count = (int)(money / Coin[i]);
                 if (count != 0)
-                    Console.WriteLine("Count of {0} baht :{1}",Coin[i],Math.Round(count));
+                    Console.WriteLine("Count of {0} baht :{1}",Coin[i],count);
                 money %= Coin[i];
     }
 
